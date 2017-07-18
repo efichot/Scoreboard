5e4400d3031257698f92885b7dccb6fcac17bf39
Etienne Fichot <fichotetienne@gmail.com>
2017-07-18 13:19:54 +0200

    Stats added && state in Application now

--- app.jsx
+++ app.jsx
@@ -16,30 +16,57 @@ const PLAYERS = [
     },
 ]
 
+function Stats(props) {
+    return (
+        <table className="stats">
+            <tbody>
+                <tr>
+                    <td>Players:</td>
+                    <td>{props.totalPlayers}</td>
+                </tr>
+                <tr>
+                    <td>Total Points:</td>
+                    <td>{props.totalScore}</td>
+                </tr>
+            </tbody>
+        </table>
+    )
+}
+
+
+Stats.propTypes = {
+    totalPlayers: React.PropTypes.number.isRequired,
+    totalScore: React.PropTypes.number.isRequired
+}
+
 function Header(props) {
     return (
         <div className="header">
+            <Stats totalPlayers={props.totalPlayers} totalScore={props.totalScore}/>
             <h1>{props.title}</h1>
         </div>
     );
 }
 
 Header.propTypes = {
-    title: React.PropTypes.string.isRequired
+    title: React.PropTypes.string.isRequired,
+    totalPlayers: React.PropTypes.number.isRequired,
+    totalScore: React.PropTypes.number.isRequired
 }
 
 function Counter(props) {
     return (
             <div className="counter">
-                <button className="counter-action decrement"> - </button>
+                <button className="counter-action decrement" onClick={function() {props.onChange(-1);}}> - </button>
                 <div className="counter-score"> {props.score} </div>
-                <button className="counter-action increment"> + </button>
+                <button className="counter-action increment" onClick={function() {props.onChange(1);}}> + </button>
             </div>
         );
 }
 
 Counter.propTypes = {
-    score: React.PropTypes.number.isRequired
+    score: React.PropTypes.number.isRequired,
+    onChange: React.PropTypes.func.isRequired
 }
 
 function Player(props) {
@@ -49,7 +76,7 @@ function Player(props) {
                 {props.name}
             </div>
             <div className="player-score">
-                <Counter score={props.score}/>
+                <Counter score={props.score} onChange={props.onChange} />
             </div>
         </div>
     );
@@ -57,7 +84,8 @@ function Player(props) {
 
 Player.propTypes = {
     name: React.PropTypes.string.isRequired,
-    score: React.PropTypes.number.isRequired
+    score: React.PropTypes.number.isRequired,
+    onChange: React.PropTypes.func.isRequired    
 }
 
 const Application = React.createClass({
@@ -73,7 +101,6 @@ const Application = React.createClass({
    getDefaultProps: function() {
        return ({
          title: "Scoreboard",
-         initialPlayers: PLAYERS
        });
    },
 
@@ -83,18 +110,31 @@ const Application = React.createClass({
        }
    },
 
+   onChange: function(delta, i) {
+        this.state.players[i].score = (delta === 1) ? this.state.players[i].score + 1 : this.state.players[i].score - 1;
+        this.setState(this.state);
+   },
+
+   totalScore: function() {
+       let totalScore = 0;
+       this.state.players.map(function(player, i) {
+        totalScore += player.score;
+       }.bind(this))
+       return totalScore;
+   },
+
     render: function() {
         return (
         <div className="scoreboard">
-            <Header title={this.props.title}/>
+            <Header title={this.props.title} totalPlayers={this.state.players.length} totalScore={this.totalScore()} />
             <div className="players">
-                 {this.state.players.map(function(player) {
-                    return <Player name={player.name} score={player.score} key={player.id} />
-                })} 
+                 {this.state.players.map(function(player, i) {
+                    return <Player name={player.name} score={player.score} key={player.id} onChange={function(delta) {this.onChange(delta, i)}.bind(this)} />
+                }.bind(this))}
             </div>
         </div>
     );
     }
 });
 
-ReactDOM.render(<Application/>, document.getElementById('container'));
+ReactDOM.render(<Application initialPlayers={PLAYERS} />, document.getElementById('container'));
